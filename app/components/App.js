69e0d7ec359f2083ab112c91a853d72ad91b69f7
Eddine <eddine.djerboua@gmail.com>
2016-11-11 17:27:47 +0100

    What

---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import { Component } from 'react';
+
+export default class App extends Component {
+  render () {
+    return <h1>OHO</h1>
+  }
+}
