0c87a1cea48851a0b40b5a38db1cf9a0ea401d1e
Eddine <eddine.djerboua@gmail.com>
2016-11-13 18:25:54 +0100

    config webpack modifs to make it fit heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,53 @@
+// var webpack = require('webpack');
+// var HTMLWebpackPlugin = require('html-webpack-plugin');
+// var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+//   template: __dirname + '/app/index.html',
+//   filename: 'index.html',
+//   inject: 'body'
+// });
+//
+// module.exports = {
+//   entry: __dirname + '/app/index.js',
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: [ 'es2015', 'react', 'react-hmre' ]
+//         }
+//       },
+//       {
+//           test: /\.scss$/,
+//           loaders: [ 'style', 'css', 'sass' ]
+//       },
+//       {
+//         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+//         loader: 'url-loader?limit=100000'
+//       }
+//     ]
+//   },
+//   output: {
+//     filename: 'transformed.js',
+//     path: __dirname + '/build ',
+//     publicPath: '/'
+//   },
+//   plugins: [
+//     new webpack.HotModuleReplacementPlugin(),
+//     HTMLWebpackPluginConfig
+//   ]
+// };
+
+
+
 var webpack = require('webpack');
-var HTMLWebpackPlugin = require('html-webpack-plugin');
-var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+var path = require('path');
 
 module.exports = {
-  entry: __dirname + '/app/index.js',
+  entry: [
+    __dirname + '/app/index.js'
+  ],
   module: {
     loaders: [
       {
@@ -28,13 +68,21 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    extensions: ['', '.js']
+  },
   output: {
-    filename: 'transformed.js',
-    path: __dirname + '/build ',
-    publicPath: '/'
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './build',
+    hot: true
   },
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    HTMLWebpackPluginConfig
+    new webpack.NoErrorsPlugin()
   ]
 };
