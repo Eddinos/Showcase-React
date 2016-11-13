0429e3acd989acbc04759d8ff4cecea15ddaffb5
Eddine <eddine.djerboua@gmail.com>
2016-11-13 17:44:11 +0100

    Deployment config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,43 +1,73 @@
-const path = require('path')
-const webpack = require('webpack')
+// const path = require('path')
+// const webpack = require('webpack')
+//
+// module.exports = {
+//   devtool: 'source-map',
+//
+//   entry: [
+//     './app/index'
+//   ],
+//
+//   output: {
+//     path: path.join(__dirname, 'build'),
+//     filename: 'transformed.js',
+//     publicPath: '/build/'
+//   },
+//
+//   plugins: [
+//     new webpack.optimize.DedupePlugin(),
+//     new webpack.optimize.UglifyJsPlugin({
+//       minimize: true,
+//       compress: {
+//         warnings: false
+//       }
+//     }),
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         'NODE_ENV': JSON.stringify('production')
+//       }
+//     })
+//   ],
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: [ 'es2015', 'react', 'react-hmre' ]
+//         }
+//       },
+//       {
+//           test: /\.scss$/,
+//           loaders: [ 'style', 'css', 'sass' ]
+//       },
+//       {
+//         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+//         loader: 'url-loader?limit=100000'
+//       }
+//     ]
+//   }
+// }
 
-module.exports = {
-  devtool: 'source-map',
 
-  entry: [
-    './src/index'
-  ],
+var config = require('./webpack.config.js');
+var webpack = require('webpack');
 
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
-  },
+config.plugins.push(
+  new webpack.DefinePlugin({
+    "process.env": {
+      "NODE_ENV": JSON.stringify("production")
+    }
+  })
+);
 
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ],
-  module: {
-    loaders: [
-      { test: /\.js?$/,
-        loader: 'babel',
-        exclude: /node_modules/ },
-      { test: /\.scss?$/,
-        loader: 'style!css!sass',
-        include: path.join(__dirname, 'src', 'styles') },
-      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'}
-    ]
-  }
-}
+config.plugins.push(
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  })
+);
+
+module.exports = config;
