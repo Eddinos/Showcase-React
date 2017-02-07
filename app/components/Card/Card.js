0ec1e5bb66189a115a504ccb507799e211f280d4
Eddine <eddine.djerboua@gmail.com>
2017-02-07 17:32:12 +0100

    Card comp style improvements
    sick transitions

--- app/components/Card/Card.js
+++ app/components/Card/Card.js
@@ -6,7 +6,7 @@ export default class Card extends Component {
   render () {
     return (
       <figure className="card">
-        <img className="card-img" src={this.props.source} alt=""/>
+        <img className="card-img" src={this.props.source} alt={this.props.title}/>
         <figcaption className="card-caption">
           <h1>{this.props.title}</h1>
           <p>{this.props.description}</p>
