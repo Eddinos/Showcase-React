e5b9d200dd395a4eb166063a11bc2e9b3dba1f8d
Eddine <eddine.djerboua@gmail.com>
2017-04-17 23:45:27 +0200

    Clean portfolio

--- app/components/Portfolio/Portfolio.js
+++ app/components/Portfolio/Portfolio.js
@@ -21,20 +21,14 @@ export default class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      projects: []
+      proyectos: []
     }
   }
 
   componentDidMount() {
     axios.get('https://snt-backend.herokuapp.com/api/getProjects')
     .then((response) => {
-      // let ninos = React.Children.map(this.props.children, child => {
-      //   if (React.isValidElement(child)) {
-      //     return React.cloneElement(child, { parentData: response.data })
-      //   }
-      // })
-      console.log(response.data);
-      this.setState({proyectos: response.data.projects})
+      this.setState({projects: response.data.projects})
     })
     .catch((error) => {
       console.log(error.message);
@@ -44,25 +38,26 @@ export default class Portfolio extends Component {
    var loadedProjects = [];
    loadedProjects.push(
    {
-     source: "http://www.basketusa.com/wp-content/uploads/2017/02/durant-harden-westbrook-2-570x325.jpg",
-     title: 'Another one',
-     description: "Wow much cool great job such engineer"
-   },
-   {
-     source: "http://www.basketusa.com/wp-content/uploads/2017/02/davis-asg.jpg",
-     title: "The last project",
-     description: "Wow much cool great job such engineer"
-   },
-   {
-     source: "http://www.basketusa.com/wp-content/uploads/2017/02/derrick-rose-2-1-570x325.jpg",
-     title: "The last project",
-     description: "Wow much cool great job such engineer"
-   });
-   this.setState({projects: loadedProjects});
+     source: "http://www.basketusa.com/wp-content/uploads/2017/03/kobe-shaq.jpg",
+     title: 'Kobe & Shaq',
+     description: "They're here for testing purpose only ! Don't click !"
+  //  },
+  //  {
+  //    source: "http://www.basketusa.com/wp-content/uploads/2017/02/davis-asg.jpg",
+  //    title: "The last project",
+  //    description: "Wow much cool great job such engineer"
+  //  },
+  //  {
+  //    source: "http://www.basketusa.com/wp-content/uploads/2017/02/derrick-rose-2-1-570x325.jpg",
+  //    title: "The last project",
+  //    description: "Wow much cool great job such engineer"
+   }
+ );
+   this.setState({proyectos: loadedProjects});
   }
 
   render () {
-    if (!this.state.proyectos) {
+    if (!this.state.projects) {
       return (
         <div className="portfolio">
           <figure className="portfolio-header">
@@ -96,7 +91,7 @@ export default class Portfolio extends Component {
           <DataContainer url="http://numbersapi.com/random/trivia"> */}
 
         <Presenter>
-          {this.state.proyectos.map((item, key) => {
+          {this.state.projects.map((item, key) => {
               return (
                 <Link to={"/portfolio/project/" + item.id + "#project"} key={key}>
                   <Card
@@ -107,7 +102,7 @@ export default class Portfolio extends Component {
                 </Link>
               )
             })}
-          {this.state.projects.map((item, key) => {
+          {this.state.proyectos.map((item, key) => {
             return (
               <NavLink to="/pikachu" key={key}>
                 <Card
@@ -127,18 +122,13 @@ export default class Portfolio extends Component {
               transitionEnterTimeout={500}
               transitionLeaveTimeout={500}
             >
-                {React.cloneElement(React.Children.only(this.props.children), {key: page, projects: this.state.proyectos })}
+                {React.cloneElement(React.Children.only(this.props.children), {key: page, projects: this.state.projects })}
             </ReactCSSTransitionGroup>
           </div>
         }
 
 
         {/* </DataContainer> */}
-
-        {/* <DataContainer url="http://numbersapi.com/random/year">
-
-        </DataContainer> */}
-
       </div>
     )
   }
