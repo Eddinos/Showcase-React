107498ab76ab1175a1f119879b89a334b221d5b4
Eddine <eddine.djerboua@gmail.com>
2017-06-22 23:49:51 +0200

    transition delay makes it sweet af

--- app/components/Skills/Skills.js
+++ app/components/Skills/Skills.js
@@ -23,7 +23,7 @@ export default class Skills extends Component {
         {this.state.skills ? (
           <div className="content">
             {this.state.skills.map((item, key) => {
-              let barStyle = this.state.trigger ? {width: `${item.value}%`} : {width: `0%`};
+              let barStyle = this.state.trigger ? {width: `${item.value}%`, transitionDelay: `0.${key%3}s`} : {width: `0%`};
               return (<div className="content-text" key={key}>{item.techno}: <div className="progress"><div className="bar" style={barStyle}></div></div></div>)
             })}
           </div>) : <div className="content content-text">Oh no ! All my skills have disappeared ! </div> }
