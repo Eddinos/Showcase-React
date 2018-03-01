42b5d1f79eb254212f8eb1a89bf5b92841940d25
Eddine Djerboua <eddine.djerboua@gmail.com>
2018-03-02 00:05:22 +0100

    Redux store sent even on ssr pages

--- webpack.config.js
+++ webpack.config.js
@@ -71,9 +71,8 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    })
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ]
 };
